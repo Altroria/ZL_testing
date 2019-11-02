b0fe4c357e21db4ffa4307661309adc13eb9585f
sunH <495648123@qq.com>
2019-11-02 17:09:19 +0800

    1

---
+++ case_pytest/Test_pytest.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+@pytest.mark.pytestto
+class TestPytestDemo(object):
+    def setup_class(self):
+        pass
+
+    @pytest.mark.asserttest
+    def test_assert(self):
+        assert 3 < 4, "3期望是小于4"
+
+    @pytest.mark.strtest
+    def test_str(self):
+        b = "hello"
+        assert "h" in b, "字符h期望在单词hello中出现"
